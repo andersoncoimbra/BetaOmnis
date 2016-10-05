547ddea18c4fdec571252235d8699fea311150ea
Anderson Coimbra <andersonmegax@hotmail.com>
2016-10-05 14:56:13 -0300

    Adaptação da listagem de reembolso da tela de financeiro do job

--- resources/views/list/listreembolso.blade.php
+++ resources/views/list/listreembolso.blade.php
@@ -1,9 +1,9 @@
 <table class="table table-striped">
-                            <tr><th>Parceiro</th><th>Job</th><th>Valor</th><th>Data</th><th>Ações</th></tr>
+                            <tr><th>Descrição</th><th>Valor</th><th>Data</th><th>Ações</th></tr>
                             @forelse($reembolsos as $reembolso)
-                                <tr><td>{{$reembolso->parceiro}}</td><td>
-                                        {{substr($reembolso->job, 0, 32)}}
-                                        @if(strlen($reembolso->job) > 31)
+                                <tr><td>
+                                        {{substr($reembolso->obs, 0, 32)}}
+                                        @if(strlen($reembolso->obs) > 31)
                                             ...
                                             @endif
                                     </td><td>{{$reembolso->valor}}</td><td>{{date('d / m / Y', strtotime($reembolso->data))}}</td><td><input type="button" class="btn btn-info" value="Detalhes" onclick="detalhesreembolso({{$reembolso->id}})" style="margin-right: 2px; margin-left: 2px" ><input type="button" class="btn btn-primary" value="Check-in" onclick="checkinreembolso({{$reembolso->id}})" style="margin-right: 2px; margin-left: 2px" >
