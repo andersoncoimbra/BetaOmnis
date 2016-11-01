e7042bcdb4dec9072a442e8dcdcf69533c7715ca
Anderson Coimbra <andersonmegax@hotmail.com>
2016-11-01 18:13:59 -0300

    Pequena Correção

--- resources/views/list/listajobs.blade.php
+++ resources/views/list/listajobs.blade.php
@@ -12,12 +12,17 @@
 @endsection
 <table class="table table-striped" id="listajobs">
     <tr><th>ID</th> <th>Job</th> <th>Parceiro</th> <th>Inicio</th> <th>Fim</th>  <th>Status</th> <th>Ações</th></tr>
+
     @forelse($jobs as $job)
         <tr><td>{{$job->id}}</td> <td>{{$job->nomeJob}}</td>
-            <td>{{$job->parceiros->nome}}</td>
+            <td>{{$job->parceiros->nome or "Sem Parceiro"}}</td>
             <td>{{date('d/m/Y', strtotime($job->inicio))}}</td>
-            <td>{{date('d/m/Y', strtotime($job->fim))}}</td> <td>{{$ds[$job->status]}}</td> <td><a href="{{url()->current()}}/{{$job->id}}"> <button type="button" class="btn btn-danger">Detalhes</button></a></td></tr>
+            <td>{{date('d/m/Y', strtotime($job->fim))}}</td>
+            <td>{{$ds[$job->status]}}</td>
+            <td><a href="{{url()->current()}}/{{$job->id}}">
+                    <button type="button" class="btn btn-danger">Detalhes</button></a></td></tr>
     @empty
         <tr><td>Sem Jobs</td></tr>
     @endforelse
+
 </table>
